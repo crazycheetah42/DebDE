baa6517b0250d6ff944211fe753e6f0ebd2ed270
Aryaman Sriram <76403763+crazycheetah42@users.noreply.github.com>
2022-12-09 14:16:38 -0500

    Update debde.sh

--- debde.sh
+++ debde.sh
@@ -14,9 +14,6 @@ if [[ "$installation_mode" == "1" ]]; then
     sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
     sudo apt update
     sudo apt install brave-browser libreoffice htop gimp vlc code virtualbox clamav clamav-daemon simplescreenrecorder pitivi geary caffeine ulauncher neofetch -y
-    echo "Your desktop has been installed. Please restart your computer to see the changes."
-    echo "Thank you for using DebDE Installer."
-    exit 1
 if [[ "$installation_mode" == "2" ]]; then
     sudo apt install kde-plasma-desktop -y
     sudo apt install apt-transport-https curl software-properties-common wget -y
@@ -24,9 +21,6 @@ if [[ "$installation_mode" == "2" ]]; then
     echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
     sudo apt update
     sudo apt install brave-browser clamav clamav-daemon simplescreenrecorder pitivi steam lutris wine-y
-    echo "Your desktop has been installed. Please restart your computer to see the changes."
-    echo "Thank you for using DebDE Installer."
-    exit 1
 if [[ "$installation_mode" == "3" ]]; then
     sudo apt install kde-plasma-desktop -y
     sudo apt install apt-transport-https curl software-properties-common wget -y
@@ -34,9 +28,6 @@ if [[ "$installation_mode" == "3" ]]; then
     echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
     sudo apt update
     sudo apt install brave-browser -y
-    echo "Your desktop has been installed. Please restart your computer to see the changes."
-    echo "Thank you for using DebDE Installer."
-    exit 1
 else
     echo "Please put an option from the list:"
     echo "1. Full Desktop (Installs apps for daily use along with desktop environment)"
@@ -44,3 +35,6 @@ else
     echo "3. Minimal Desktop (Installs a web browser with desktop environment)"
     read installation_mode
 fi
+echo "Your desktop has been installed. Please restart your computer to see the changes."
+echo "Thank you for using DebDE Installer."
+exit 1
